7dddb420c2811006789f2dcbe649fad53734d3a4
shuntagami <shuntagami23@gmail.com>
2020-12-24 16:54:48 +0900

    位置を変更

--- network.tf
+++ network.tf
@@ -78,6 +78,19 @@ resource "aws_route_table" "private_1" {
   vpc_id = aws_vpc.example.id
 }
 
+# リスト7.11, 7.16 プライベートのルートの定義
+resource "aws_route" "private_0" {
+  route_table_id         = aws_route_table.private_0.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_0.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "private_1" {
+  route_table_id         = aws_route_table.private_1.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_1.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
 resource "aws_route_table_association" "private_0" {
   subnet_id      = aws_subnet.private_0.id
   route_table_id = aws_route_table.private_0.id
@@ -112,15 +125,3 @@ resource "aws_nat_gateway" "nat_gateway_1" {
   depends_on    = [aws_internet_gateway.example]
 }
 
-# リスト7.11, 7.16 プライベートのルートの定義
-resource "aws_route" "private_0" {
-  route_table_id         = aws_route_table.private_0.id
-  nat_gateway_id         = aws_nat_gateway.nat_gateway_0.id
-  destination_cidr_block = "0.0.0.0/0"
-}
-
-resource "aws_route" "private_1" {
-  route_table_id         = aws_route_table.private_1.id
-  nat_gateway_id         = aws_nat_gateway.nat_gateway_1.id
-  destination_cidr_block = "0.0.0.0/0"
-}
