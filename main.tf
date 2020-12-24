b1e55f312c47b16a50f8ff30f67adbe1b287884d
shuntagami <shuntagami23@gmail.com>
2020-12-24 17:52:25 +0900

    albの定義
    セキュリティグループはmoduleを使って定義

--- main.tf
+++ main.tf
@@ -1,8 +1,24 @@
-module "dev_server" {
-  source        = "./http_server"
-  instance_type = "t2.micro"
+# リスト8.2 ALB のセキュリティグループの定義
+module "http_sg" {
+  source      = "./security_group"
+  name        = "http-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 80
+  cidr_blocks = ["0.0.0.0/0"]
 }
 
-output "public_dns" {
-  value = module.dev_server.public_dns
+module "https_sg" {
+  source      = "./security_group"
+  name        = "https-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 443
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "http_redirect_sg" {
+  source      = "./security_group"
+  name        = "http-redirect-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 8080
+  cidr_blocks = ["0.0.0.0/0"]
 }
