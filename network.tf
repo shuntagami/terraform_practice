b24aed9609cd8c64e7adb4aec5f134d75a521861
shuntagami <shuntagami23@gmail.com>
2020-12-24 15:58:38 +0900

    サブネット、インターネットゲートウェイ、ルートテーブルの作成

--- network.tf
+++ network.tf
@@ -8,3 +8,34 @@ resource "aws_vpc" "example" {
     Name = "example"
   }
 }
+
+# リスト7.2
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1a"
+}
+
+# リスト7.3
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+}
+
+# リスト7.4
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.example.id
+}
+
+# リスト7.5
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+# リスト7.6
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
