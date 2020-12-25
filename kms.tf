d63ef9724425a9e1cef3c7c812fdbf0a693f5597
shuntagami <shuntagami23@gmail.com>
2020-12-25 17:01:43 +0900

    Create kms.tf

---
+++ kms.tf
@@ -0,0 +1,13 @@
+# カスタマーマスターキーの定義
+resource "aws_kms_key" "example" {
+  description             = "Example Customer Master Key"
+  enable_key_rotation     = true
+  is_enabled              = true
+  deletion_window_in_days = 30
+}
+
+# リスト11.2 エイリアスの定義
+resource "aws_kms_alias" "example" {
+  name          = "alias/example"
+  target_key_id = aws_kms_key.example.key_id
+}
