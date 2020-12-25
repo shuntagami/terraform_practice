83c1f7b886633463b206312f200f350ad9464cc3
shuntagami <shuntagami23@gmail.com>
2020-12-25 14:38:45 +0900

    タスク実行ロールの定義

--- main.tf
+++ main.tf
@@ -1,3 +1,31 @@
+terraform {
+  required_version = "~> 0.14.3"
+}
+
+# リスト9.6 AmazonECSTaskExecutionRolePolicy の参照
+data "aws_iam_policy" "ecs_task_execution_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# リスト9.7 ECS タスク実⾏IAM ロールのポリシードキュメントの定義
+data "aws_iam_policy_document" "ecs_task_execution" {
+  source_json = data.aws_iam_policy.ecs_task_execution_role_policy.policy
+
+  statement {
+    effect    = "Allow"
+    actions   = ["ssm:GetParameters", "kms:Decrypt"]
+    resources = ["*"]
+  }
+}
+
+# リスト9.8 ECS タスク実⾏IAM ロールの定義
+module "ecs_task_execution_role" {
+  source     = "./iam_role"
+  name       = "ecs-task-execution"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy     = data.aws_iam_policy_document.ecs_task_execution.json
+}
+
 # リスト8.2 ALB のセキュリティグループの定義
 module "http_sg" {
   source      = "./security_group"
